a7db3a19ee313147d9427dd3e4668ddc21e472eb
Sagar Kalbande <sagarkalbande1993@gmail.com>
2020-04-25 04:38:51 +0530

    added linux version

--- LearningOpenGL/main.cpp
+++ LearningOpenGL/main.cpp
@@ -1,47 +1,28 @@
-//Jeff Chastine
-#include <Windows.h>
-#include <GL\glew.h>
-#include <GL\freeglut.h>
-#include <iostream>
+#include <GL/freeglut.h>
+#include <GL/gl.h>
 
-using namespace std;
-
-void changeViewPort(int w, int h)
+void renderFunction()
 {
-    glViewport(0, 0, w, h);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(-0.5, 0.5);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
 }
-
-void render()
+int main(int argc, char** argv)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glutSwapBuffers();
-}
-
-
-
-int main(int argc, char* argv[]) {
-
-    // Initialize GLUT
     glutInit(&argc, argv);
-    // Set up some memory buffers for our display
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-    // Set the window size
-    glutInitWindowSize(800, 600);
-    // Create the window with the title "Hello,GL"
-    glutCreateWindow("Hello, GL");
-    // Bind the two functions (above) to respond when necessary
-    glutReshapeFunc(changeViewPort);
-    glutDisplayFunc(render);
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-    // Very important!  This initializes the entry points in the OpenGL driver so we can 
-    // call all the functions in the API.
-    GLenum err = glewInit();
-    if (GLEW_OK != err) {
-        fprintf(stderr, "GLEW error");
-        return 1;
-    }
-
-
-    glutMainLoop();
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("OpenGL - First window demo");
+    glutDisplayFunc(renderFunction);
+    glutMainLoop();    
     return 0;
 }
