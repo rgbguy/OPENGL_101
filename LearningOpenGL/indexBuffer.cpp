aab8d287af07f84d8df454a5669b3578990e5f25
Sagar Kalbande <sagarkalbande1993@gmail.com>
2020-04-28 00:55:51 +0530

    index buffer

---
+++ LearningOpenGL/indexBuffer.cpp
@@ -0,0 +1,68 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(){
+
+    if(!glfwInit()){
+        //error
+    }
+
+    glewExperimental = GL_TRUE;
+
+    glewInit();
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "VertexAttrib", NULL, NULL);
+
+    if(!window){
+        //error
+    }
+
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    float positions[8] = {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f
+    };
+
+    unsigned int index[6] = {
+        0, 1, 2,
+        2, 3, 0,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
+
+    unsigned int indexbuffer;
+    glGenBuffers(1, &indexbuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(unsigned int), index, GL_STATIC_DRAW);
+
+    while(!glfwWindowShouldClose(window)){
+
+        glfwPollEvents();   
+
+        //glDrawArrays(GL_TRIANGLES, 0, 4);   
+        //glDrawElements when we give index buffer. 6 is the number of indices
+        //last param is nullptr as we have already bound the index buffer
+        //otherwise we would have to give index buffer pointer
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); 
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
